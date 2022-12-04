60221273899d5c1d1ed4500ae1aea586988835e4
Nico Freundt <nico.freundt@protonmail.com>
2022-12-04 12:33:04 +0100

    solution for day four

---
+++ 04/main.py
@@ -0,0 +1,19 @@
+def read_range_pairs():
+    with open("input.txt") as file:
+        return [[{'min': int(r.split('-')[0]), 'max': int(r.split('-')[1])} for r in line.split(',')] for line in file.readlines()]
+
+
+def count_range_subsets(range_pairs):
+    return sum([set(range(pair[0]['min'], pair[0]['max']+1)).issubset(set(range(pair[1]['min'], pair[1]['max']+1))) or set(range(pair[0]['min'], pair[0]['max']+1)).issuperset(set(range(pair[1]['min'], pair[1]['max']+1))) for pair in range_pairs])
+
+
+def count_range_overlapping(range_pairs):
+    return sum([not set(range(pair[0]['min'], pair[0]['max']+1)).isdisjoint(set(range(pair[1]['min'], pair[1]['max']+1))) for pair in range_pairs])
+
+
+if __name__ == "__main__":
+    # part one
+    print(count_range_subsets(read_range_pairs()))
+
+    # part two
+    print(count_range_overlapping(read_range_pairs()))
