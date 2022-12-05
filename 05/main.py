917241b1aee5a196de5cb5f104444f0c74bcf47c
Nico Freundt <nico.freundt@protonmail.com>
2022-12-05 15:39:14 +0100

    cleanup day five

--- 05/main.py
+++ 05/main.py
@@ -4,7 +4,7 @@ import re
 
 def read_stacks():
     with open("input.txt") as file:
-        return [[e[1] for e in stack if ' ' not in e] for stack in zip(*[[line[i:i + 3] for i in range(0, len(line), 4)] for line in file.readlines() if '[' in line][::-1])]
+        return [[e[1] for e in stack if re.match(r'\S', e)] for stack in zip(*[[line[i:i + 3] for i in range(0, len(line), 4)] for line in file.readlines() if re.match(r'( *\[[A-Z]] *)+', line)][::-1])]
 
 
 def read_procedures():
@@ -14,15 +14,13 @@ def read_procedures():
 
 def crane_9000(procedure_list, stack_list):
     for procedure in procedure_list:
-        stack_list[procedure[2]-1] += stack_list[procedure[1]-1][-procedure[0]:][::-1]
-        stack_list[procedure[1]-1] = stack_list[procedure[1]-1][:-procedure[0]]
+        stack_list[procedure[2] - 1] += [stack_list[procedure[1] - 1].pop() for _ in range(procedure[0])]
     return ''.join([stack[-1] for stack in stack_list])
 
 
 def crane_9001(procedure_list, stack_list):
     for procedure in procedure_list:
-        stack_list[procedure[2]-1] += stack_list[procedure[1]-1][-procedure[0]:]
-        stack_list[procedure[1]-1] = stack_list[procedure[1]-1][:-procedure[0]]
+        stack_list[procedure[2] - 1] += [stack_list[procedure[1] - 1].pop() for _ in range(procedure[0])][::-1]
     return ''.join([stack[-1] for stack in stack_list])
 
 
